ae484ebb26d66f0fd38e52e710ca295a76fadcf5
Mikhail Mikhaliov <legiar@gmail.com>
2012-01-13 14:54:12 +0200

    Network scaffold. Poppulate ip addresses after create network.

--- app/models/ip_address.rb
+++ app/models/ip_address.rb
@@ -1,3 +1,17 @@
 class IpAddress < ActiveRecord::Base
   belongs_to :network
+  belongs_to :slice
+  
+  validates :network, :presence => true
+  validates :ip, :ip_address => true
+  
+  after_initialize :set_default_values
+  
+  private
+  
+  def set_default_values
+    if new_record?
+      self.state = 'free'
+    end
+  end
 end
--- app/models/network.rb
+++ app/models/network.rb
@@ -5,17 +5,18 @@ class Network < ActiveRecord::Base
   has_many :addresses, :class_name => 'IpAddress'
   
   validates :name, :presence => true, :uniqueness => true
-  validates :address, :presence => true, :uniqueness => true, :ip_address => true, :allow_cidr => true
-  validates :gateway, :presence => true, :ip_address => true
-  #validates :begin_ip, :ip_address => true
-  #validates :end_ip, :ip_address => true
+  validates :zone, :presence => true
+  validates :address, :uniqueness => true, :ip_address => {:v4 => true, :allow_cidr => true}
+  validates :gateway, :ip_address => true
+  validates :dns1, :ip_address => true
+  validates :dns2, :ip_address => true
+  validates :begin_ip, :ip_address => true
+  validates :end_ip, :ip_address => true
   
-  attr_writer :begin_ip, :end_ip  
+  attr_accessor :begin_ip, :end_ip
+  
+  after_initialize :set_default_values  
   after_create :create_ip_addresses
-  
-  def begin_ip
-    
-  end
   
   protected
   
@@ -23,9 +24,17 @@ class Network < ActiveRecord::Base
     addr = IPAddr.new(begin_ip)
     end_addr = IPAddr.new(end_ip)
     while addr <= end_addr
-      addresses << NetworkAddress.create(:ip => addr.to_s)
+      addresses << IpAddress.create(:ip => addr.to_s)
       addr = addr.succ
     end
     save
+  end
+  
+  private
+  
+  def set_default_values
+    if new_record?
+      self.mode = "static"
+    end
   end
 end
