86e314f6c64fe272cd39f589adc4a480e1187bd5
Mikhail Mikhaliov <legiar@gmail.com>
2012-01-19 15:49:11 +0200

    Fix capistrano deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,67 +1,58 @@
+require "bundler/capistrano"
+require "capistrano_colors"
+
+set :application, "HyperCloud.Web"
 set :rails_env, "production"
 
-set :application, "hyperadmin"
+set :deploy_to, "/home/webmaster/#{application}"
+set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
-set :repository, "git://github.com/legiar/hyperadmin.git"
 set :scm, :git
+set :git_enable_submodules, 1
+set :repository, "git://github.com/legiar/HyperCloud.Web.git"
 set :branch, "master"
 set :deploy_via, :remote_cache
-#set :git_enable_submodules, 1
 
-role :app, "zabbix.bpmonline.com"
-#role :db, "zabbix.bpmonline.com", :primary => true
-
-set :user, "hyperadmin"
+set :user, "webmaster"
 set :use_sudo, false
+host = "cloud.bpmonline.com"
 
-set :app_dir, "/home/#{user}"
-#set :copy_dir, "#{app_dir}/tmp"
+role :web, host
+role :app, host
+role :db,  host, :primary => true
 
-set :deploy_to, "#{app_dir}"
-
-set :keep_releases, 10
-
-after 'deploy:update_code', 'config:symlink_app_config'
-
-namespace :deploy do
-  task :start do
-  end
-
-  task :stop do
-  end
-
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-
-  after "deploy:setup" do
-    run "mkdir -p #{deploy_to}/shared/pids && mkdir -p #{deploy_to}/shared/config && mkdir -p #{deploy_to}/shared/var"
-  end
-
-  after "deploy:update" do
-    deploy::cleanup
-  end
-end
+after "deploy:update", "deploy:cleanup"
+after "deploy:update_code", "config:symlink_app_config"
+after "deploy:symlink", "config:symlink_app_config"
+before "deploy:assets:precompile", "bundle:install"
+before "deploy:assets:precompile", "config:symlink_app_config"
 
 namespace :config do
   task :symlink_app_config do
     run "ln -sf #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
-    run "ln -sf #{deploy_to}/shared/menu_helper #{current_release}/vendor/plugins/menu_helper"
+    #run "ln -sf #{deploy_to}/shared/config/unicorn.rb #{current_release}/config/unicorn.rb"
   end
 end
 
-namespace :unicorn do
+namespace :deploy do
+  task :restart, :roles => :app do
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
+  end
+  
   task :start do
-    #run "export PATH=/opt/ree/bin:$PATH && cd #{deploy_to}/current && unicorn_rails -c #{deploy_to}/current/config/unicorn.rb -e #{rails_env} -D"
-    run "export PATH=/opt/ree/bin:$PATH && cd #{deploy_to}/current && unicorn_rails -c #{deploy_to}/current/config/unicorn.rb -D"
+    run "cd #{current_path} && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
   end
-
+  
   task :stop do
-    run "kill -9 `cat #{deploy_to}/shared/pids/unicorn.pid`"
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
   end
-
-  task :restart do
-    run "kill -HUP `cat #{deploy_to}/shared/pids/unicorn.pid`"
+  
+  after "deploy:setup" do
+    run "mkdir -p #{deploy_to}/shared/config"
   end
 end
+
+after 'deploy:update',  'bundle:install'
+#after 'deploy:restart', 'unicorn:stop'
 
