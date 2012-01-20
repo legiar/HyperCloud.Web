b86683a73dde2ec2fa8472f8e4881a52d22a9386
Mikhail Mikhaliov <legiar@gmail.com>
2012-01-20 09:48:23 +0200

    Add showing timeline (last 30 days) by site error log

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,2 +1,12 @@
 class SitesController < InheritedResources::Base
+  respond_to :html, :json
+
+  def show
+    show! do
+      @timeline = ((Time.zone.now - 30.days).to_date..(Time.zone.now - 1.day).to_date).inject([]){ |accum, date| accum << date }  
+      @timeline_log = SiteErrorLog.timeline(resource.name).count 
+      @max_count = 0
+      @timeline_log.each_value {|v| @max_count = v unless v <= @max_count}
+    end
+  end  
 end
--- app/models/site_error_log.rb
+++ app/models/site_error_log.rb
@@ -1,4 +1,6 @@
 class SiteErrorLog < ActiveRecord::Base
   establish_connection "log"
   set_table_name "log4net"
+  
+  scope :timeline, lambda {|site| where(:site => site).where('`date` >= ? and `date` <= ?',  Time.zone.now.beginning_of_day - 30.days, Time.zone.now.end_of_day - 1.day).group('date(`date`)')}
 end
