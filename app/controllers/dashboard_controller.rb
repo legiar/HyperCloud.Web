8ac4084e3eea485ab50949fc6ac6c5c5ebcc2d81
Mikhail Mikhaliov <legiar@gmail.com>
2012-01-20 23:03:07 +0200

    Remove stale code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,29 +1,8 @@
 class DashboardController < ApplicationController
-  before_filter :set_current_tab
   
   def index
     @timeline = ((Time.zone.now - 30.days).to_date..(Time.zone.now - 1.day).to_date).inject([]){ |accum, date| accum << date }
-    @timeline_site_errors = SiteErrorLog.full_timeline.count
-    
-    #@active_tickets = Ticket.not_closed.active_tickets
-    #@closed_tickets = Ticket.closed_tickets
-
-    # create array of date strings from 30 days ago up to yesterday
-    #
-
-    #@timeline_opened_tickets = Ticket.timeline_opened_tickets
-    #@timeline_closed_tickets = Ticket.timeline_closed_tickets
-
-    #@max_opened = 0
-    #@max_closed = 0
-
-    #@timeline_opened_tickets.each_value do |v|
-    #  @max_opened = v unless v <= @max_opened
-    #end
-
-    #@timeline_closed_tickets.each_value do |v|
-    #  @max_closed = v unless v <= @max_closed
-    #end    
+    @timeline_site_errors = SiteError.full_timeline.count
   end
 
   protected
@@ -37,10 +16,4 @@ class DashboardController < ApplicationController
       end
     end
     
-  private 
-  
-    def set_current_tab
-      @current_tab = :dashboard
-    end  
-  
 end
