b8e6b27875adcbb96358bac0d06b5e40ca11e5ed
Mikhail Mikhaliov <legiar@gmail.com>
2012-01-20 22:47:48 +0200

    Add helpers for use sorting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 
   before_filter :authenticate_user!
   before_filter :set_time_zone
+  
+  helper_method :sort_column, :sort_direction
 
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
@@ -17,11 +19,22 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def layout_by_resource
-    devise_controller? ? "devise" : "application"
-  end
+    def layout_by_resource
+      devise_controller? ? "devise" : "application"
+    end
+    
+    def set_time_zone
+      #Time.zone = @current_user.time_zone if @current_user
+    end
+    
+    def sort_column
+      # TODO: How get resource_class from InheritedResource  
+      #resource_class.column_names.include?(params[:sort]) ? params[:sort] : resource_class.primary_key
+      params[:sort] ? params[:sort] : "id"
+    end  
   
-  def set_time_zone
-    #Time.zone = @current_user.time_zone if @current_user
-  end
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"  
+    end      
+  
 end
