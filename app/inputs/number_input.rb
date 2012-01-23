2fed5f165cbed63269ec419150e27918db3d09c7
Mikhail Mikhaliov <legiar@gmail.com>
2012-01-23 10:16:31 +0200

    Redesign stylesheets for formtastic
    Change css for use formtastic.
    Switch to use formtastic in devise views.

--- app/inputs/number_input.rb
+++
@@ -1,8 +0,0 @@
-class NumberInput < Formtastic::Inputs::NumberInput
-  
-  def to_html
-    template.content_tag(:dt, label_html) << 
-    template.content_tag(:dd, builder.text_field(method, input_html_options))
-  end
-  
-end
--- app/inputs/select_input.rb
+++
@@ -1,8 +0,0 @@
-class SelectInput < Formtastic::Inputs::SelectInput
-  
-  def to_html
-    template.content_tag(:dt, label_html) << 
-    template.content_tag(:dd, options[:group_by] ? grouped_select_html : select_html)
-  end
-
-end
