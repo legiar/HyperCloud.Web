45e5620b86569b86bee9efef753cfb8eb14f64f6
Mikhail Mikhaliov <legiar@gmail.com>
2012-01-26 16:24:29 +0200

    Fix slices scaffold display

--- app/controllers/slices_controller.rb
+++ app/controllers/slices_controller.rb
@@ -1,2 +1,14 @@
 class SlicesController < InheritedResources::Base
+  
+  protected
+  
+    def collection
+      @slices ||= end_of_association_chain.order(sort_column + ' ' + sort_direction).paginate(
+        :per_page => 10,
+        :page => params[:page])
+    end
+
+    def default_sort_column
+      "name"
+    end
 end
