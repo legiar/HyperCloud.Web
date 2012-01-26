708e7591e9c923200e543a97c1a84eb25ab79339
Mikhail Mikhaliov <legiar@gmail.com>
2012-01-26 17:57:50 +0200

    Add Pool to related models

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,6 +2,7 @@ class Site < ActiveRecord::Base
   belongs_to :customer
   belongs_to :database
   belongs_to :slice
+  belongs_to :pool
   has_many :site_errors, :foreign_key => :site, :primary_key => :name
 
   validates :name, :presence => true, :uniqueness => true
--- app/models/slice.rb
+++ app/models/slice.rb
@@ -2,6 +2,7 @@ class Slice < ActiveRecord::Base
   belongs_to :host
   belongs_to :active_host, :class_name => 'Host'
   belongs_to :network_address
+  belongs_to :pool
   has_many :sites
 
   validates :name, :presence => true, :uniqueness => true
