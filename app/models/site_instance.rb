be6b6114c90f66e1e1dcb96487cf8227c892817a
Mikhail Mikhaliov <legiar@gmail.com>
2012-01-26 20:51:37 +0200

    Add default values to SiteInstance

--- app/models/site_instance.rb
+++ app/models/site_instance.rb
@@ -1,4 +1,11 @@
 class SiteInstance < ActiveRecord::Base
   belongs_to :site
   belongs_to :slice
+  
+  after_initialize do |model|
+    if new_record?
+      model.state = "Unknown"
+      model.enabled = false
+    end
+  end
 end
