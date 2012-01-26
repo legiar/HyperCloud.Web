3d470a3778ff1d752b6db34b3b53fef03f520d60
Mikhail Mikhaliov <legiar@gmail.com>
2012-01-26 13:45:55 +0200

    Fix disaplay hosts scaffold

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,2 +1,14 @@
 class HostsController < InheritedResources::Base
+  
+  protected
+  
+    def collection
+      @hosts ||= end_of_association_chain.order(sort_column + ' ' + sort_direction).paginate(
+        :per_page => 10,
+        :page => params[:page])
+    end
+
+    def default_sort_column
+      "name"
+    end
 end
